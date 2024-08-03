8930afaa6bbb7a56bbe32cabc0b14cae974b54cb
Ashik Eqbal <hello@itsashik.info>
2024-08-04 04:03:53 +0530

    Update footer component styling and information

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer
-            className="bg-zinc-900 h-16 flex items-center justify-center text-white fixed bottom-0 w-full"
+            className="bg-zinc-900 h-10 flex items-center justify-center text-white fixed bottom-0 w-full"
         >
             <p>
                 Created with ❤️ by {'Ashik Eqbal'}

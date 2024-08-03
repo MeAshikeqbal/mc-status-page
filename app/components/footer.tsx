bdf637d91ea0974954b83fd3b57fed16a244f1a9
Ashik Eqbal <hello@itsashik.info>
2024-08-04 04:01:37 +0530

    Update footer component with additional information and styling

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,19 @@
 export default function Footer() {
     return (
-        <footer>
-        <p>Footer</p>
+        <footer
+            className="bg-zinc-900 h-16 flex items-center justify-center text-white fixed bottom-0 w-full"
+        >
+            <p>
+                Created with ❤️ by {'Ashik Eqbal'}
+            </p>
+            <span className="mx-2">|</span>
+            <p>
+                Last updated on {new Date().toLocaleTimeString()}
+            </p>
+            <span className="mx-2">|</span>
+            <p>
+                view source code on <a href="https://github.com/MeAshikeqbal/mc-status-page">GitHub</a>
+            </p>
         </footer>
     );
 }
